97fea937674634aed343db1a28ff285ff0437977
Frank Morgner <frankmorgner@gmail.com>
2018-08-21 09:40:19 +0200

    fixed showing password prompt with PAM_SILENT

--- src/pam_p11.c
+++ src/pam_p11.c
@@ -107,7 +107,13 @@ int prompt(int flags, pam_handle_t *pamh, int style, char **response,
 {
 	int r;
 
-	if (PAM_SILENT == (flags & PAM_SILENT)) {
+	if (PAM_SILENT == (flags & PAM_SILENT)
+			&& style != PAM_TEXT_INFO
+			&& style != PAM_PROMPT_ECHO_OFF) {
+		/* PAM_SILENT does not override the prompting of the user for passwords
+		 * etc., it only stops informative messages from being generated. We
+		 * use PAM_TEXT_INFO and PAM_PROMPT_ECHO_OFF exclusively for the
+		 * password prompt. */
 		r = PAM_SUCCESS;
 	} else {
 		va_list args;
