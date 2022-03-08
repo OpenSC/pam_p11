b973b0e413e9d421cedd0e8231aeccf230c8a74c
Frank Morgner <frankmorgner@gmail.com>
2022-03-08 15:45:04 +0100

    added openssl 3 compatibility for ec pubkeys

--- src/match_openssh.c
+++ src/match_openssh.c
@@ -110,6 +110,68 @@ static EVP_PKEY *init_evp_pkey_rsa(BIGNUM *rsa_e, BIGNUM *rsa_n)
 	return key;
 }
 
+static EVP_PKEY *init_evp_pkey_ec(int nid_curve, BIGNUM *pub_x, BIGNUM *pub_y)
+{
+	EVP_PKEY *key = NULL;
+
+	if (!pub_x || !pub_y)
+		return NULL;
+
+#if OPENSSL_VERSION_NUMBER < 0x30000000L
+	key = EVP_PKEY_new();
+	if (!key)
+		return NULL;
+
+	EC_KEY *ec_key = EC_KEY_new_by_curve_name(nid_curve);
+	if (!ec_key) {
+		EVP_PKEY_free(key);
+		return NULL;
+	}
+
+	/* do error checking here: valid x, y, ec_key, point on curve.. */
+	if (!EC_KEY_set_public_key_affine_coordinates(ec_key, pub_x, pub_y)) {
+		EC_KEY_free(ec_key);
+		EVP_PKEY_free(key);
+		return NULL;
+	}
+	EVP_PKEY_assign_EC_KEY(key, ec_key);
+#else
+	OSSL_PARAM_BLD *bld = NULL;
+	OSSL_PARAM *params = NULL;
+	EVP_PKEY_CTX *pctx = NULL;
+	char *group_name;
+	switch (nid_curve) {
+		case NID_X9_62_prime256v1:
+			group_name = SN_X9_62_prime256v1;
+			break;
+		case NID_secp384r1:
+			group_name = SN_secp384r1;
+			break;
+		case NID_secp521r1:
+			group_name = SN_secp521r1;
+			break;
+		default:
+			return NULL;
+	}
+
+	if ((pctx = EVP_PKEY_CTX_new_from_name(NULL, "EC", NULL)) == NULL
+			|| (bld = OSSL_PARAM_BLD_new()) == NULL
+			|| !OSSL_PARAM_BLD_push_utf8_string(bld, OSSL_PKEY_PARAM_GROUP_NAME, group_name, 0)
+			|| !OSSL_PARAM_BLD_push_BN(bld, OSSL_PKEY_PARAM_EC_PUB_X, pub_x)
+			|| !OSSL_PARAM_BLD_push_BN(bld, OSSL_PKEY_PARAM_EC_PUB_Y, pub_y)
+			|| (params = OSSL_PARAM_BLD_to_param(bld)) == NULL
+			|| EVP_PKEY_fromdata_init(pctx) <= 0
+			|| EVP_PKEY_fromdata(pctx, &key, EVP_PKEY_PUBLIC_KEY, params) <= 0) {
+		EVP_PKEY_CTX_free(pctx);
+		OSSL_PARAM_free(params);
+		OSSL_PARAM_BLD_free(bld);
+		return NULL;
+	}
+#endif
+
+	return key;
+}
+
 static EVP_PKEY *ssh1_line_to_key(char *line)
 {
 	EVP_PKEY *key = NULL;
@@ -289,7 +351,6 @@ err:
 static EVP_PKEY *ssh_nistp_line_to_key(char *line)
 {
 	EVP_PKEY *key;
-	EC_KEY *ec_key;
 	BIGNUM *x;
 	BIGNUM *y;
 
@@ -378,24 +439,19 @@ static EVP_PKEY *ssh_nistp_line_to_key(char *line)
 		return NULL;
 	i++;
 
-	/* create key */
-	ec_key = EC_KEY_new_by_curve_name(nid);
-
 	/* read point coordinates */
 	x = BN_bin2bn(decoded + i, flen, NULL);
 	i += flen;
 	y = BN_bin2bn(decoded + i, flen, NULL);
 
-	/* do error checking here: valid x, y, ec_key, point on curve.. */
-	if (!EC_KEY_set_public_key_affine_coordinates(ec_key, x, y)) {
-		EC_KEY_free(ec_key);
-		BN_free(x);
-		BN_free(y);
-		return NULL;
+	key = init_evp_pkey_ec(nid, x, y);
+	if (!key) {
+		if (x)
+			BN_free(x);
+		if (y)
+			BN_free(y);
 	}
 
-	key = EVP_PKEY_new();
-	EVP_PKEY_assign_EC_KEY(key, ec_key);
 	return key;
 }
 
