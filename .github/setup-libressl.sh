8cc32c60a373a0c41143ddb2e77939a6ee165a99
Peter Popovec <popovec.peter@gmail.com>
2023-05-19 09:28:18 +0200

    libressl CI test (test-login)
    	new file:   .github/setup-libressl.sh
    	modified:   .github/workflows/ci.yml

---
+++ .github/setup-libressl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -ex -o xtrace
+
+V=libressl-3.6.1
+
+# keep libp11-dev installed!
+sudo dpkg --purge --force-depends libssl-dev
+
+if [ ! -d "$V" ]; then
+	# letsencrypt CA does not seem to be included in CI runner
+	wget --no-check-certificate https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/$V.tar.gz
+	tar xzf $V.tar.gz
+fi
+pushd $V
+./configure --prefix=/usr/local
+make -j $(nproc)
+sudo make install
+popd
+
+# update dynamic linker to find the libraries in non-standard path
+echo "/usr/local/lib64" | sudo tee /etc/ld.so.conf.d/openssl.conf
+sudo ldconfig
