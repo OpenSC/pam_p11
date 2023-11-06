609081118931b160963489b7acf71c9eceb37226
Doug Heckman <55454330+archxlith@users.noreply.github.com>
2023-11-06 07:26:17 -0500

    Update pam_p11.c - key_login() always succeeded (#32)
    key_login always succeed even if PIN input was incorrect. r = PAM_SUCCESS prior to key_login and didn't get changed to a failure if key_login failed.

--- src/pam_p11.c
+++ src/pam_p11.c
@@ -699,8 +699,10 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t * pamh, int flags, int argc,
 		goto err;
 	}
 
-	if (1 != key_login(pamh, flags, authslot, pin_regex))
+	if (1 != key_login(pamh, flags, authslot, pin_regex)) {
+		r = PAM_CRED_INSUFFICIENT;
 		goto err;
+	}
 
 	if (authkey == NULL && authcert) {
 		if (NULL == (authkey = PKCS11_find_key(authcert))) {
